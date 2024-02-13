b18c07f52dc19060a3688bc52ce8648dc63e0c5c
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-02-13 17:43:09 +0000

    importdata and export data

---
+++ Basics/importexportdata/module.js
@@ -0,0 +1,2 @@
+const {people,ages}=require("./people");
+console.log(people,ages)//thats how we can use multiple data from a diff file to another file
---
+++ Basics/importexportdata/people.js
@@ -0,0 +1,7 @@
+
+people=["jack","vizie","analyia","halbert"]
+ages=[24,22,28,26]
+
+console.log(people,ages)
+//now we wanna import this data to module file 
+module.exports={people,ages}
