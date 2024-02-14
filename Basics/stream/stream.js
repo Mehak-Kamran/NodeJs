bc798cb2f8581135ada5118200fedb020e75c8a6
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-02-14 11:41:02 +0000

    create and write streams

--- Basics/stream/stream.js
+++ Basics/stream/stream.js
@@ -1,9 +1,16 @@
 const fs=require("fs")
 
 let readstream=fs.createReadStream("Basics/stream/blog.txt",{encoding:"utf8"})
+writestream=fs.createWriteStream("Basics/stream/blog1.txt")
 
 readstream.on("data",(chunks)=>{
     console.log("----new chunk-----")
     console.log(chunks)
+    writestream.write("----newchunk----")
+     writestream.write(chunks)
 
 })
+
+//writestream
+
+
