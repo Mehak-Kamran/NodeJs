f6d0f7d15b972dbb75f88735ece905b0657a5375
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-02-16 17:47:24 +0000

    html page through node js

--- Basics/server/server.js
+++ Basics/server/server.js
@@ -11,14 +11,36 @@
 
 //req res
 
+// let http=require("http")
+// let server=http.createServer((req,res)=>{
+//     //console.log(req.url,req.method)
+//     //res.setHeader("Content-Type","text/plain")
+//     res.setHeader("Content-Type","text/html")
+//     res.write("<p>hi there</p>")
+//     res.write("<p>hi there again</p>")
+//     res.end()
+// })
+
+// server.listen(3000,"localhost",()=>{
+//     console.log("server has listened")
+// })
+
+//sending html file
+
 let http=require("http")
+let fs=require("fs")
 let server=http.createServer((req,res)=>{
-    //console.log(req.url,req.method)
-    //res.setHeader("Content-Type","text/plain")
     res.setHeader("Content-Type","text/html")
-    res.write("<p>hi there</p>")
-    res.write("<p>hi there again</p>")
-    res.end()
+    fs.readFile("Basics/server/view/index.html",(err,data)=>{
+        if(err){
+           console.log(err)
+        }
+        else{
+           res.write(data)
+           res.end()
+        }
+    
+})
 })
 
 server.listen(3000,"localhost",()=>{
