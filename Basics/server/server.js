73d1a063e53e0ad67aed7b1d0382ba649b0364ab
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-02-16 17:33:44 +0000

    res req

--- Basics/server/server.js
+++ Basics/server/server.js
@@ -1,6 +1,24 @@
+//server
+
+// let http=require("http")
+// let server=http.createServer((req,res)=>{
+//     console.log("req has been made")
+// })
+
+// server.listen(3000,"localhost",()=>{
+//     console.log("server has listened")
+// })
+
+//req res
+
 let http=require("http")
 let server=http.createServer((req,res)=>{
-    console.log("req has been made")
+    //console.log(req.url,req.method)
+    //res.setHeader("Content-Type","text/plain")
+    res.setHeader("Content-Type","text/html")
+    res.write("<p>hi there</p>")
+    res.write("<p>hi there again</p>")
+    res.end()
 })
 
 server.listen(3000,"localhost",()=>{
